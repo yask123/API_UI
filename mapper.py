f59d85d578cc9f31e23651d56f79a7ec17ef5bf1
Yask Srivastava <yask123@gmail.com>
2015-12-31 10:20:31 +0530

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,87 +1,141 @@
+
 import requests
 import ast 
 import json
-token=''
+token='CAACEdEose0cBAJhv0w1yRVXrbrSqZAF8wkZACNf6S3AogUAgdZBiee9FVmQEZA1Df70l291UulkWU7i8B4omZAoa66utA8fmxjZAJhbUMnlgP841TUeEbHg3YqpDW84dZB4odfDM8J52JgMMzZAl8kVMIHXEwWCbbKTfRjjNksUF6bwcJLWQGSH0yKnSyIr5Y9ZAsxxMUyJ4K5AZDZD'
 import yaml
 
-
-name= 'starbucks'
-location='new york'
 url = 'http://127.0.0.1:8000/'+token
 r = requests.post("http://localhost:8000/", data = {"name":name,"location":location,"fbtoken":token})
 d = json.loads(r.text.strip())
-
+d =  ast.literal_eval(d)
 
 google = d['google']['results']
 facebook = d['facebook']
 fb_location = yaml.safe_load(facebook['location'].strip())
 fb_new_loc={}
-for each in fb_location:
-	print each.replace('u','')[1:-1] 
-exit()
-for each in fb_location:
-	print each.replace('u','')[1:]
-exit()	
-if bool(each.replace('u','')[1:-1] == 'state'):
-	fb_new_loc['state'] = fb_location[each]	
-elif bool(each.replace('u','')[1:-1] == 'latitude'):		
-	fb_new_loc['latitude'] = fb_location[each]
-	print 'sdadasdasd'
-elif bool(each.replace('u','')[1:-1] == 'longitude'):		
-	fb_new_loc['longitude'] = fb_location[each]
-elif bool(each.replace('u','')[1:-1] == 'street'):		
-	fb_new_loc['street'] = fb_location[each]
-elif bool(each.replace('u','')[1:-1] == 'zip'):		
-	fb_new_loc['zip'] = fb_location[each]
-elif bool(each.replace('u','')[1:-1] == 'city'):		
-	fb_new_loc['city'] = fb_location[each]				
-print fb_new_loc['latitude']	
 
-
+common = ['address','timing','latitude','AddressCountry','Og:type','zipcode','name','AddressRegion','longitude','source']
 
 g_mapped={}
-g_mapped['address']=google[0]['vicinity']
-g_mapped['name'] = google[0]['name']
-g_mapped['timing'] = google[0]['opening_hours']
-g_mapped['longitude'] = google[0]['geometry']['location']['lng']
-g_mapped['latitude'] = google[0]['geometry']['location']['lat']
-g_mapped['Og:image'] = google[0]['photos'][0]['photo_reference']
-g_mapped['Og:image:height'] = google[0]['photos'][0]['height']
-g_mapped['Og:image:width'] = google[0]['photos'][0]['width']
-g_mapped['types'] = google[0]['types']
-g_mapped['icon'] = google[0]['icon']
-g_mapped['rating'] = google[0]['rating']
-
-
 fb_mapped={}
 
+for each in common:
+	g_mapped[each]=''
+for each in common:
+	fb_mapped[each]=''
+
+try:
+	g_mapped['address']=google[0]['vicinity']
+except:
+	pass
+try:
+	g_mapped['name'] = google[0]['name']
+except:
+	pass
+try:
+	g_mapped['timing'] = google[0]['opening_hours']
+except:
+	pass
+try:
+	g_mapped['longitude'] = google[0]['geometry']['location']['lng']
+except:
+	pass
+try:
+	g_mapped['latitude'] = google[0]['geometry']['location']['lat']
+except:
+	pass
+try:
+	g_mapped['Og:type'] = google[0]['types']
+except:
+	pass	
+try:
+	g_mapped['Og:image'] = google[0]['photos'][0]['photo_reference']
+	g_mapped['Og:image:height'] = google[0]['photos'][0]['height']
+	g_mapped['Og:image:width'] = google[0]['photos'][0]['width']
+except:
+	pass	
 
 
 
+try:
+	g_mapped['types'] = google[0]['types']
+except:
+	pass
+try:
+	g_mapped['icon'] = google[0]['icon']
+except:
+	pass
+try:
+	g_mapped['rating'] = google[0]['rating']
+except:
+	pass
+try:
+	fb_mapped['website'] = facebook['website']
+except:
+	pass	
+try:				
+	fb_mapped['fb_page_url'] = facebook['fb_page_url']
+except:
+	pass
+try:				
+	fb_mapped['fb_page_likes'] = facebook['likes']
+except:
+	pass
+try:				
+	fb_mapped['fbpage_is_verified'] = facebook['is_verified']
+except:
+	pass
+try:
+	fb_mapped['parking'] = facebook['parking']
+except:
+	pass
+try:
+	fb_mapped['name'] = facebook['name']
+except:
+	pass	
+try:
+	fb_mapped['phone'] = facebook['phone']
+except:
+	pass
+try:			
+	fb_location = yaml.safe_load(facebook['location'].strip())
+except:
+	pass
+for each in fb_location:
+	try:
+		if bool(each.replace('u','')[1:-1] == 'state'):
+			fb_mapped['state']  = fb_location[each]	
+		elif bool(each.replace('u','')[1:-1] == 'latitde'):		
+			fb_mapped['latitude'] = fb_location[each]
+		elif bool(each.replace('u','')[1:-1] == 'longitde'):		
+			fb_mapped['longitude'] = fb_location[each]
+		elif bool(each.replace('u','')[1:-1] == 'street'):		
+			fb_mapped['address'] = fb_location[each]
+		elif bool(each.replace('u','')[1:-1] == 'zip'):		
+			fb_mapped['zipcode'] = fb_location[each]
+		elif bool(each.replace('u','')[1:-1] == 'city'):		
+			fb_mapped['city'] = fb_location[each]
+		elif bool(each.replace('u','')[1:-1] == 'country'):		
+			fb_mapped['AddressCountry'] = fb_location[each]
+	except:
+		pass					
 
-
-
-
-fb_location = yaml.safe_load(facebook['location'])
-
-
-fb_mapped['website'] = facebook['website']
-fb_mapped['fb_page_url'] = facebook['fb_page_url']
-fb_mapped['fb_page_likes'] = facebook['likes']
-fb_mapped['parking'] = facebook['parking']
-fb_mapped['fbpage_is_verified'] = facebook['is_verified']
-fb_mapped['parking'] = facebook['parking']
-fb_mapped['phone'] = facebook['phone']
-fb_mapped['state'] = fb_location['state']
-fb_mapped['latitude'] = fb_location['latitude']
-fb_mapped['longitude'] = fb_location['longitude']
-fb_mapped['AddressCountry'] = fb_location['country']
-fb_mapped['city'] = fb_location['city']
-fb_mapped['address'] = fb_location['street']
-fb_mapped['zipcode'] = fb_location['zip']	
 complete_map={}
-complete_map={'google':g_mapped,'facebook':fb_mapped}
-
-
-
+fb_uncommon=[]
+g_uncommon=[]
+for each in fb_mapped:
+	if each not in common:
+		fb_uncommon.append(each)
+for each in g_mapped:
+	if each not in common:
+		g_uncommon.append(each)	
+fb_mapped['source'] = 'Facebook Pages'
+g_mapped['source'] = 'Google Places API'	
+fb_mapped['longitude'] = fb_mapped['longitude'][len("Decimal('"):-2]
+fb_mapped['latitude'] = fb_mapped['latitude'][len("Decimal('"):-2]
+fb_mapped['zipcode'] = fb_mapped['zipcode'][2:-1]	
+fb_mapped['address'] = fb_mapped['address'][2:-1]
+		
+complete_map={'google':g_mapped,'facebook':fb_mapped,'common':common,'fb_uncommon':fb_uncommon,'g_uncommon':g_uncommon}
 
